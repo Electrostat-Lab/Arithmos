c8b8cf7af6873299c0b6b5cb4bea97e99ef41523
Scrappers <scrappers.tm@gmail.com>
2022-01-02 22:31:19 -0600

    Refractored to the 24-bit-RGB Color System

--- build/compile/compile.sh
+++ build/compile/compile.sh
@@ -14,16 +14,20 @@ source buildGroovy.sh
 source buildJava.sh
 source buildNatives.sh
 
+echo -e $RESET_Cs
+
 if [[ $enable_scala_build == true ]]; then
-    echo -e "$WHITE_C---MajorTask@Build Scala : Scala build started"
+    echo -e "$BRIGHT_RED_C---MajorTask@Build Scala : Scala build started"
     copyScSources
     if [[ `compileScala` -eq 0 ]]; then
         echo -e "$GREEN_C Task@Build Compile Scala : Succeeded"
     else 
         echo -e "$RED_C Task@Build Compile Scala : Failed"
     fi
-    echo -e "$WHITE_C---MajorTask@Build Scala : Scala build finished"
+    echo -e "$BRIGHT_RED_C---MajorTask@Build Scala : Scala build finished"
 fi
+
+echo -e $RESET_Cs
 
 if [[ $enable_kt_build == true ]]; then
     echo -e "$VIOLET_C---MajorTask@Build Kotlin : Kotlin build started"
@@ -36,6 +40,8 @@ if [[ $enable_kt_build == true ]]; then
     echo -e "$VIOLET_C---MajorTask@Build Kotlin : Kotlin build finished"
 fi
 
+echo -e $RESET_Cs
+
 if [[ $enable_groovy_build == true ]]; then
     echo -e "$CYAN_C---MajorTask@Build Groovy : Groovy build started"
     copyGroovySources
@@ -47,8 +53,10 @@ if [[ $enable_groovy_build == true ]]; then
     echo -e "$CYAN_C---MajorTask@Build Groovy : Groovy build finished"
 fi
 
+echo -e $RESET_Cs
+
 if [[ $enable_java_build == true ]]; then
-    echo -e "$YELLOW_C---MajorTask@Build Java Sources : Java build started"
+    echo -e "$ORANGE_C---MajorTask@Build Java Sources : Java build started"
     copyJavaSources
     if [[ `generateHeaders` -eq 0 ]]; then
        moveHeaders
@@ -56,14 +64,18 @@ if [[ $enable_java_build == true ]]; then
     else
         echo -e "$RED_C Task@Build Compile Java : Failed"
     fi
-    echo -e "$YELLOW_C---MajorTask@Build Java Sources : Java build finished"
+    echo -e "$ORANGE_C---MajorTask@Build Java Sources : Java build finished"
 fi
+
+echo -e $RESET_Cs
 
 if [[ $enable_natives_build == true ]]; then
-    echo -e "$DARK_GREY_C---MajorTask@Build Native Sources : Native build started"
+    echo -e "$MAGNETA_C---MajorTask@Build Native Sources : Native build started"
     copyNativeSources
     compile
-    echo -e "$DARK_GREY_C---MajorTask@Build Native Sources : Native build finished"
+    echo -e "$MAGNETA_C---MajorTask@Build Native Sources : Native build finished"
 fi
 
-echo -e "\e[0m--------Script end--------"
+echo -e $RESET_Cs
+
+echo "--------Script end--------"
--- build/compile/variables.sh
+++ build/compile/variables.sh
@@ -4,15 +4,26 @@
 #* @author pavl_g.
 #*#
 # Bash colors using ANSI 
-
+# favored the 24-bit (3 colors * 8-bit for each one<255>) color system
 # Colors
-RED_C='\033[1;31m'
-GREEN_C='\033[1;32m'
-WHITE_C='\e[1;37m'
-YELLOW_C='\e[1;33m'
+# process failure color
+RED_C='\033[38;2;255;50;50m'
+# success color
+GREEN_C='\e[38;2;0;180;0m'
+# extra
+WHITE_C='\e[38;2;255;255;255m'
+# Java color
+ORANGE_C='\e[38;2;250;155;0m'
+# C alternative color
 DARK_GREY_C='\e[1;30m'
-VIOLET_C='\e[1;35m'
-CYAN_C='\e[1;36m'
+# kotlin color
+VIOLET_C='\e[38;2;217;80;223m'
+# groovy color
+CYAN_C='\e[38;2;0;155;255m'
+# C++ color
+MAGNETA_C='\e[38;2;170;150;150m'
+# Scala color
+BRIGHT_RED_C='\e[38;2;200;120;120m'
 # Highlights
 RED_H='\033[1;41m'
 GREEN_H='\033[1;42m'
@@ -21,6 +32,7 @@ WHITE_H='\e[1;47m'
 RED_C_F='\033[1;5;41m'
 GREEN_C_F='\033[1;5;42m'
 WHITE_C_F='\e[1;5;47m'
+RESET_Cs='\033[0;0m'
 
 
 # define work directory
